2689deca3dafbed75655604a8a307f93f6a54921
Sean Lim <lim_kai_xiang_sean@s2013.sst.edu.sg>
2018-04-09 02:23:56 +0800

    Initial commit

---
+++ app/initialize.js
@@ -0,0 +1,20 @@
+import Typed from 'typed.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  // do your setup here
+  console.log('Initialized app');
+
+  var options = {
+    strings: ["Welcome to NP's Hack Club", "Welcome to Overflow"],
+    startDelay: 1000,
+    showCursor: true,
+    cursorChar: '_',
+    autoInsertCss: true,
+    smartBackspace: true,
+    backSpeed: 80,
+    backDelay: 50,
+    typeSpeed: 100
+  }  
+
+  var typed = new Typed(".element", options);
+});
